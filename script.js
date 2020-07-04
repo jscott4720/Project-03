bdd66916e975dfb5359f489e37e6f343b3afd7ec
Jeremiah <jscott4720@gmail.com>
2020-07-04 17:21:25 -0400

    Added images from api

---
+++ script.js
@@ -0,0 +1,17 @@
+url = "https://rickandmortyapi.com/api/character/";
+
+rickIMG = document.querySelector(".rick");
+mortyIMG = document.querySelector(".morty");
+
+allcharacters = document.querySelectorAll(".characters");
+let ul = document.querySelector("ul");
+let li = document.querySelectorAll("li");
+let img = document.querySelectorAll("img");
+
+fetch(url)
+  .then((res) => res.json())
+  .then((res) => {
+    for (i = 0; i < res.results.length; i++) {
+      img[i].src = res.results[i].image;
+    }
+  });
